d3411266015f466a8e0f356216287d5213b75c7b
Deadx34 <isurushehan393@gmail.com>
2025-11-26 23:59:55 +0530

    Refactor database connection to load configuration from server_config.php

--- db_connect.php
+++ db_connect.php
@@ -1,23 +1,21 @@
 <?php
-$servername = "localhost";
-$username = "b33_40185301"; // Change this to your cPanel DB username
-$password = "123456";     // Change this to your cPanel DB password
-$dbname = "b33_40185301_rent_and_go"; // Change this to your DB name
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Load config if available, otherwise fall back to hard-coded defaults
+$configFile = __DIR__ . '/server_config.php';
+if (file_exists($configFile)) {
+    $config = require $configFile;
+    $servername = $config['db_host'] ?? 'localhost';
+    $username = $config['db_user'] ?? 'b33_40185301'; // Change this to your cPanel DB username
+    $password = $config['db_pass'] ?? '123456';      // Change this to your cPanel DB password
+    $dbname = $config['db_name'] ?? 'b33_40185301_rent_and_go'; // Change this to your DB name
+} else {
+    $servername = "localhost";
+    $username = "b33_40185301"; // Change this to your cPanel DB username
+    $password = "123456";     // Change this to your cPanel DB password
+    $dbname = "b33_40185301_rent_and_go"; // Change this to your DB name
 }
-<?php
-$config = require __DIR__ . '/server_config.php';
-
-$servername = $config['db_host'];
-$username = $config['db_user'];
-$password = $config['db_pass'];
-$dbname = $config['db_name'];
 
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
