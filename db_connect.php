578a91818755ab47a3da43f767dfab651336ec4a
Deadx34 <isurushehan393@gmail.com>
2025-11-27 00:20:15 +0530

    Update database connection details in db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,15 +1,10 @@
 <?php
-// 1. HOSTNAME: Copy this EXACTLY from your cPanel "MySQL Host Name"
-// It is usually NOT "localhost" on ByetHost/InfinityFree.
-$servername = "sql302.byethost.com"; 
+$servername = "sql104.byethost33.com"; 
 
-// 2. USERNAME: This is your cPanel username (from the error: b33_40185301)
 $username = "b33_40185301"; 
 
-// 3. PASSWORD: Your hosting account/vPanel password
 $password = "123456"; 
 
-// 4. DB NAME: Usually includes your username prefix (e.g., b33_40185301_rent_and_go)
 $dbname = "b33_40185301_rent_and_go"; 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
